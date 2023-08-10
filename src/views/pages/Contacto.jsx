f7b66e245b455496ad684cb7f412473d04aef475
Mauricio Espinoza P <espinoza.p.mauricio@gmail.com>
2023-08-10 01:16:53 -0400

    Contacto: delete sweetalert

--- src/views/pages/Contacto.jsx
+++ src/views/pages/Contacto.jsx
@@ -3,8 +3,8 @@ import { collection, addDoc } from 'firebase/firestore'
 import { db } from '../../config/firebase';
 
 // import Swal from 'sweetalert2/dist/sweetalert2.all.min.js';
-import withReactContent from 'sweetalert2-react-content';
-import Swal from 'sweetalert2';
+// import withReactContent from 'sweetalert2-react-content';
+// import Swal from 'sweetalert2';
 // import Button from "react-bootstrap/Button";
 // import Form from "react-bootstrap/Form";
 // import Card from "react-bootstrap/Card";
@@ -20,12 +20,12 @@ export const Contacto = () => {
   //referencia collection firebase
   const contactoCollectionRef = collection(db, 'contactos');
   //  const MySwal = withReactContent(Swal);
-  const alertSwal = withReactContent(Swal.mixin({
-    customClass: {
-      confirmButton: 'btn alert-btn',
-    },
-    buttonsStyling: false
-  }));
+  // const alertSwal = withReactContent(Swal.mixin({
+  //   customClass: {
+  //     confirmButton: 'btn alert-btn',
+  //   },
+  //   buttonsStyling: false
+  // }));
   //evento submit boton
   const onSubmit = async (event) => {
     event.preventDefault();
@@ -41,21 +41,21 @@ export const Contacto = () => {
     console.log("id_contacto::" +  result.id);
     if (result.id != "") {
         //alertSwal.fire("Mensaje Enviado!", "Satisfactoriamente");
-        alertSwal.fire({
-          icon: 'success',
-          iconColor: '#32e1f1',
-          color: '#64472E',
-          titleText: 'Mensaje ha sido enviado satisfactoriamente.',
-        });
+        // alertSwal.fire({
+        //   icon: 'success',
+        //   iconColor: '#32e1f1',
+        //   color: '#64472E',
+        //   titleText: 'Mensaje ha sido enviado satisfactoriamente.',
+        // });
         ClearInput();
     } else {
         //alertSwal.fire("Hemos tenido un problema al enviar tu mensaje","" , "error");
-        alertSwal.fire({
-          icon: 'danger',
-          iconColor: '#32e1f1',
-          color: '#64472E',
-          titleText: 'Hemo tenido un problema al enviar tu mensaje.',
-        });
+        // alertSwal.fire({
+        //   icon: 'danger',
+        //   iconColor: '#32e1f1',
+        //   color: '#64472E',
+        //   titleText: 'Hemo tenido un problema al enviar tu mensaje.',
+        // });
     }
 
     function ClearInput() {
